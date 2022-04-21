8b2497c9a4e6b5c2707d0e3ed913062d1779c71e
Beeno Tung <aabbcc1241@yahoo.com.hk>
2022-04-22 07:50:10 +0800

    working on proxy with relational schema

---
+++ src/db-proxy.ts
@@ -0,0 +1,169 @@
+import { Statement } from 'better-sqlite3'
+import { DBInstance, migrateUp } from 'better-sqlite3-schema'
+
+export function proxyDB<Dict extends { [table: string]: object[] }>(
+  db: DBInstance,
+): Dict {
+  type TableName = keyof Dict
+  type Row<Name extends TableName> = Dict[Name][number]
+  let tableProxyMap = new Map<string, Dict[TableName]>()
+
+  function proxyTable<Name extends TableName>(
+    table: string,
+    fields: string[],
+  ): Dict[Name] {
+    type Table = Dict[Name]
+    if (tableProxyMap.has(table)) {
+      return tableProxyMap.get(table) as Table
+    }
+    let rowProxyMap = new Map<number, Row<Name>>()
+
+    let select_column_dict: Record<string, Statement> = {}
+    for (let field of fields) {
+      select_column_dict[field] = db
+        .prepare(/* sql */ `select ${field} from ${table} where id = ?`)
+        .pluck()
+    }
+
+    let count = db.prepare(/* sql */ `select count(*) from ${table}`).pluck()
+    let delete_by_length = db.prepare(
+      /* sql */ `delete from ${table} where id >= ?`,
+    )
+
+    let update_dict: Record<string, Statement> = {}
+    let update_run = (id: number, row: Record<string, any>) => {
+      let keys = Object.keys(row)
+      if (keys.length == 0) return
+      let key = keys.join('|')
+      let update =
+        update_dict[key] ||
+        (update_dict[key] = db.prepare(
+          /* sql */ `update ${table} set ${keys} where id = :id`,
+        ))
+      update.run(row)
+    }
+
+    let insert_empty = db.prepare(
+      /* sql */ `insert into ${table} (id) values (null)`,
+    )
+
+    let insert_dict: Record<string, Statement> = {}
+    let insert_run = (row: Record<string, any>) => {
+      let keys = Object.keys(row)
+      if (keys.length) {
+        insert_empty.run()
+        return
+      }
+      let key = keys.join('|')
+      let insert =
+        insert_dict[key] ||
+        (insert_dict[key] = db.prepare(
+          /* sql */ `insert into ${table} (${keys}) values (${keys.map(
+            key => ':' + key,
+          )})`,
+        ))
+      insert.run(row)
+    }
+
+    let count_by_id = db
+      .prepare(/* sql */ `select count(*) from ${table} where id = ?`)
+      .pluck()
+
+    let proxy = new Proxy([] as unknown[] as Table, {
+      has(target, p) {
+        console.log('has:', p)
+        if (typeof p !== 'symbol') {
+          let id = +p
+          if (Number.isInteger(id)) {
+            return count_by_id.get(id) === 1
+          }
+        }
+        return Reflect.has(target, p)
+      },
+      set(target, p, value, receiver) {
+        console.log('set:', p)
+        if (p === 'length') {
+          delete_by_length.run(value)
+          return true
+        }
+        if (typeof p !== 'symbol') {
+          let id = +p
+          if (Number.isInteger(id)) {
+            if (count_by_id.get(id) === 1) {
+              update_run(id, value)
+            } else {
+              insert_run({ id, ...value })
+            }
+            return true
+          }
+        }
+        return Reflect.set(target, p, value, receiver)
+      },
+      get(target, p, receiver) {
+        console.log('get:', p)
+        if (p === 'length') {
+          return count.get()
+        }
+        if (p === 'push') {
+          return function () {
+            for (let i = 0; i < arguments.length; i++) {
+              insert_run(arguments[i])
+            }
+            return count.get()
+          }
+        }
+        if (typeof p !== 'symbol') {
+          let id = +p
+          if (Number.isInteger(id)) {
+            return proxyRow(table, rowProxyMap, id)
+          }
+        }
+        if (p === Symbol.iterator) {
+          return function* () {
+            for (let offset = 0; ; offset++) {
+              let row = select_by_offset.get(offset)
+              if (row) {
+                yield decode(row.value)
+              } else {
+                break
+              }
+            }
+          }
+        }
+        return Reflect.get(target, p, receiver)
+      },
+    })
+    tableProxyMap.set(table, proxy)
+    return proxy
+  }
+  function proxyRow<Name extends TableName>(
+    table: string,
+    rowProxyMap: Map<number, Row<Name>>,
+    id: number,
+  ): Row<Name> {
+    if (rowProxyMap.has(id)) {
+      return rowProxyMap.get(id)
+    }
+
+    let proxy = new Proxy({} as unknown as Row<Name>, {
+      set(target, p, value, receiver) {
+        console.log('row.set:', p, value)
+        return Reflect.set(target, p, value, receiver)
+      },
+      get(target, p, receiver) {
+        console.log('row.get:', p)
+        return Reflect.get(target, p, receiver)
+      },
+    })
+    rowProxyMap.set(id, proxy)
+    return proxy
+  }
+  return new Proxy({} as Dict, {
+    get(target, propertyKey, receiver) {
+      if (typeof propertyKey === 'string') {
+        return proxyTable(propertyKey)
+      }
+      return Reflect.get(target, propertyKey, receiver)
+    },
+  })
+}
--- test/db.ts
+++ test/db.ts
@@ -1,45 +1,71 @@
 import DB from 'better-sqlite3-helper'
-import { toSafeMode } from 'better-sqlite3-schema'
 import { join } from 'path'
 import { proxyDB } from '../src/proxy'
 
 export let db = DB({
   path: join('data', 'sqlite3.db'),
-  migrate: false,
+  migrate: {
+    migrations: [
+      /* sql */ `
+-- Up
+create table if not exists user (
+  id integer primary key
+, username text not null unique
+);
+-- Down
+drop table user;
+`,
+      /* sql */ `
+-- Up
+create table if not exists post (
+  id integer primary key
+, user_id integer not null references user (id)
+, content text not null
+, created_at timestamp not null default current_timestamp
+);
+-- Down
+drop table post;
+`,
+    ],
+  },
 })
 
-toSafeMode(db)
-
 let proxy = proxyDB<{
-  users: { username: string }[]
-  posts: {
+  user: {
+    id?: number
+    username: string
+  }[]
+  post: {
+    id?: number
     user_id: number
     content: string
-    created_at: string | Date
+    created_at?: string
   }[]
 }>(db)
 
-proxy.users[1] = { username: 'alice' }
-if (proxy.users.length < 2) {
-  proxy.users[2] = { username: 'bob' }
+proxy.user[1] = { id: 1, username: 'Alice' }
+console.log('user[1]:', proxy.user[1].username)
+
+proxy.user[1] = { username: 'alice' }
+if (proxy.user.length < 2) {
+  proxy.user[2] = { username: 'bob' }
 }
-proxy.posts[1] = {
+proxy.post[1] = {
   content: 'hello world',
-  created_at: new Date(),
   user_id: 1,
 }
 // proxy.users[0] = { username: 'zero' }
 
 console.log({
-  user_1: proxy.users[1],
-  user_2: proxy.users[2],
-  user_3: proxy.users[3],
-  post_1: proxy.posts[1],
-  user_len: proxy.users.length,
+  user_1: proxy.user[1],
+  user_2: proxy.user[2],
+  user_3: proxy.user[3],
+  post_1: proxy.post[1],
+  user_len: proxy.user.length,
 })
 
-for (let a of proxy.users) {
-  for (let b of proxy.users) {
+for (let a of proxy.user) {
+  for (let b of proxy.user) {
     console.log({ a, b })
   }
 }
